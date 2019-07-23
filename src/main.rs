cf6bc98e93f866691467417d3a83fbabc5f561ff
Camilo <camilo.jorquera.a@gmail.com>
2019-07-23 10:55:58 -0400

    feat: http methods was added

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,38 @@
 
 #[macro_use] extern crate rocket;
 
-#[get("/<name>/<age>")]
-fn index(name: String, age: u8) -> String {
-    format!("Hello, {} year old named {}!", age, name)
+use rocket::response::content;
+
+#[get("/health")]
+fn health() -> content::Json<&'static str>{
+    content::Json("{'status':'UP'}")
+}
+
+#[get("/<rut>/<name>")]
+fn get_detail(name: String, rut: String) -> content::Json<String> {
+    let detail:String = format!("{{ '{}':'{}'}}", rut, name);
+    content::Json(detail)
+}
+
+#[post("/<rut>")]
+fn new_person(rut: String) -> content::Json<String> {
+    content::Json(format!("{{'created':'{}'}}", rut))
+}
+
+#[put("/<rut>")]
+fn update_person(rut: String) -> content::Json<String> {
+    content::Json(format!("{{'updated':'{}'}}", rut))
+}
+
+#[delete("/<rut>")]
+fn del_person(rut: String) -> content::Json<String> {
+    content::Json(format!("{{'deleted':'{}'}}", rut))
 }
 
 fn main() {
-    rocket::ignite().mount("/hello", routes![index]).launch();
+    rocket::ignite().mount("/gs", routes![get_detail,
+    health,
+    new_person,
+    update_person,
+    del_person]).launch();
 }
